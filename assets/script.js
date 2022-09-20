a9e53e9e19586f8cc5c4919bf307c0cc34322386
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-20 19:11:24 -0400

    fixed check answer and changed how counter worked to change questions and added timer code with not allow multiple countdowns to occur, and more

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-var chosenQuestion = "";
-var chosenAnswer = "";
+var currentQuestion = 0;
 var timeRemain = document.querySelector("#count");
 var startQuiz = document.querySelector("#startQuiz");
 var timer;
 var count;
-
+var gameStarted = false;  //allows for countdown check to stop duplicates
 
 
 var questions = [
@@ -16,12 +15,12 @@ var questions = [
     {
         question: "The condition in a if/else statement in enclosed within __.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: 3
+        answer: 2
     },
     {
         question: "Arrays in Javascript can be used to store __.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: 4
+        answer: 3
     },
     {
         question: "String values must be enclosed within __ when being assigned to variables.",
@@ -36,51 +35,55 @@ var questions = [
 ];
 
 function renderQuestion(){
-    
-    for (var i = 0; i < questions.length; i++){
 
-        chosenQuestion = document.querySelector(".questions").textContent = questions[i].question;
+    if (currentQuestion < questions.length) //fixed error when at final question
+
+        document.querySelector(".questions").textContent = questions[currentQuestion].question;
 
         //I got this outcome from this with tweaks https://stackoverflow.com/questions/11128700/create-a-ul-and-fill-it-based-on-a-passed-array
+        var list = "<button>" + questions[currentQuestion].choices.join("</button></br><button>") + "</button>";
 
-        var list = "<button><li>" + questions[i].choices.join("</li></button></br><button><li>") + "</li></button>";
+        document.querySelector(".answers").innerHTML = list;
+}
 
-        chosenAnswer = document.querySelector(".answers").innerHTML = list; //adds list to the html code
-        document.querySelector(".answers").addEventListener("click", checkAnswer(i, questions[i].answer));
-    }
+
     
+
+function checkAnswer(answer){
+    var keyAnswer = questions[currentQuestion].answer;
+    var correctAnswer = questions[currentQuestion].choices[keyAnswer];
+
+        if (answer === correctAnswer){
+            document.querySelector(".check").innerHTML = '"Correct"';
+        }else{
+            document.querySelector(".check").innerHTML = '"Incorrect"';
+            count -= 10;
+            timeRemain.textContent = count;  //needs to reinitialize the timeRemain
+        }
+    //next 2 lines is what changes the quiz question
+    currentQuestion++;
+    renderQuestion(); 
 }
 
-function checkAnswer(i, arr){
-    console.log()
-    return function(){
-        var givenAnswer = i,
-            correctAnswer = questions[i].answer;
-
-            if (givenAnswer === correctAnswer){
-                document.querySelector(".check").innerHTML = '"Correct"';
-            }else{
-                document.querySelector(".check").innerHTML = '"Incorrect"';
-            }
-            renderQuestion();
-    }
-}
 
 function start() {
-    countdown();
-    renderQuestion();
+    var startButton = document.querySelector('#startQuiz'); //https://flaviocopes.com/how-to-disable-button-javascript/
+    startButton.disabled = true;
+    startButton.style.display = 'none';  //hides the start quiz button
+    countdown();  
+    renderQuestion();  //initialze the quiz
+    document.querySelector(".answers").addEventListener("click", e => checkAnswer(e.target.textContent)); // moved to remove double renderQuestion() aka loops this line to move next question
 }
 
 function countdown(){
-    count = 75;
-    setInterval(function() {
-        count--;
-        timeRemain.textContent = count;
-
-        // if (count >= 0) {   
-        //     endQs;
-        // }
-    }, 1000);
+    if (!gameStarted) {
+        count = 75;
+        setInterval(function() {
+            count--;
+            timeRemain.textContent = count;
+        }, 1000);
+        gameStarted = true;
+    };
 }
 
 
